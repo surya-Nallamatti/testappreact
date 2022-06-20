f1bd5d27401e6abda6a42590afd3906b5a7fded0
surya-Nallamatti <102635876+surya-Nallamatti@users.noreply.github.com>
2022-06-20 13:35:43 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class HelloWorld extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('root')
+);
